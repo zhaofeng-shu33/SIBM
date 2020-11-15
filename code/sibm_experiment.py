919ff8b2b78053cd99548e21a9952b56d3686b39
zhaofeng-shu33 <616545598@qq.com>
2020-11-15 11:57:33 +0800

    debug experiment for k

--- code/sibm_experiment.py
+++ code/sibm_experiment.py
@@ -73,8 +73,63 @@ class SIBM2:
                 self._metropolis_single()
         return self.sigma
 
+class SIBMk:
+    '''SIBM with multiple community'''
+    def __init__(self, graph, _alpha, _beta, k=2):
+        self.G = graph
+        self._beta = _beta
+        self._alpha_divide_beta = _alpha / self._beta
+        self.n = len(self.G.nodes)
+        # randomly initiate a configuration
+        self.sigma = [1 for i in range(self.n)]
+        nodes = list(self.G)
+        random.Random().shuffle(nodes)
+        self.k = k
+        for node_state in range(k):
+            for i in range(self.n // k):
+                self.sigma[nodes[i * k + node_state]] = node_state
+        # node state is 0, 1, \dots, k-1
+        self.m = [self.n / k for i in range(k)] # each number of w^s
+        self.mixed_param = self._alpha_divide_beta * np.log(self.n)
+        self.mixed_param /= self.n
+    def get_dH(self, trial_location, w_s):
+        _sum = 0
+        sigma_r = self.sigma[trial_location]
+        w_s_sigma_r = (w_s + self.sigma[trial_location]) % self.k
+        for i in self.G[trial_location]:
+            if sigma_r == self.sigma[i]:
+                _sum += 1
+            elif w_s_sigma_r == self.sigma[i]:
+                _sum -= 1
+        _sum *= (1 + self.mixed_param)
+        _sum += self.mixed_param * (self.m[w_s_sigma_r] - self.m[sigma_r] + 1)
+        return _sum
+    def _metropolis_single(self):
+        # randomly select one position to inspect
+        r = random.randint(0, self.n - 1)
+         # randomly select one new flipping state to inspect
+        w_s = random.randint(1, self.k - 1)
+        delta_H = self.get_dH(r, w_s)
+        if delta_H < 0:  # lower energy: flip for sure
+            self.m[self.sigma[r]] -= 1
+            self.sigma[r] = (w_s + self.sigma[r]) % self.k
+            self.m[self.sigma[r]] += 1
+        else:  # Higher energy: flip sometimes
+            probability = np.exp(-1.0 * self._beta * delta_H)
+            if np.random.rand() < probability:
+                self.m[self.sigma[r]] -= 1
+                self.sigma[r] = (w_s + self.sigma[r]) % self.k
+                self.m[self.sigma[r]] += 1
+    def metropolis(self, N=40):
+        # iterate given rounds
+        for _ in range(N):
+            for _ in range(self.n):
+                self._metropolis_single()
+        return self.sigma
+
 def exact_compare(labels):
     # return 1 if labels = X or -X
+    print(labels)
     n2 = int(len(labels) / 2)
     labels_inner = np.array(labels)
     return np.sum(labels_inner) == 0 and np.abs(np.sum(labels_inner[:n2])) == n2
@@ -89,11 +144,14 @@ def majority_voting(labels_list):
     voting_result = np.sum(labels_np, axis=0) > 0
     return 2 * np.asarray(voting_result, dtype=int) - 1
 
-def task(repeat, n, a, b, alpha, beta, num_of_sibm_samples, m, _N, qu):
+def task(repeat, n, k, a, b, alpha, beta, num_of_sibm_samples, m, _N, qu=None):
     total_acc = 0
     for _ in range(repeat):
         G = sbm_graph(n, 2, a, b)
-        sibm = SIBM2(G, alpha, beta)
+        if k == 2:
+            sibm = SIBM2(G, alpha, beta)
+        else:
+            sibm = SIBMk(G, alpha, beta, k)
         sibm.metropolis(N=_N)
         acc = 0
         sample_list = []
@@ -103,21 +161,30 @@ def task(repeat, n, a, b, alpha, beta, num_of_sibm_samples, m, _N, qu):
         for i in range(num_of_sibm_samples):
             # collect nearly independent samples
             candidates_samples = [sample_list[i + j * num_of_sibm_samples] for j in range(m)]
-            inner_acc = int(exact_compare(majority_voting(candidates_samples))) # for exact recovery
+            if len(candidates_samples) == 1:
+                inner_acc = int(exact_compare(candidates_samples[0]))
+            else:
+                inner_acc = int(exact_compare(majority_voting(candidates_samples))) # for exact recovery
             acc += inner_acc
         acc /= num_of_sibm_samples
         total_acc += acc
     total_acc /= repeat
-    qu.put(total_acc)
+    if qu is not None:
+        qu.put(total_acc)
+        return
+    else:
+        return total_acc
 
-def get_acc(repeat, n, a, b, alpha, beta, num_of_sibm_samples, m, _N, thread_num):
+def get_acc(repeat, n, k, a, b, alpha, beta, num_of_sibm_samples, m, _N, thread_num):
+    if thread_num == 1:
+        return task(repeat, n, k, a, b, alpha, beta, num_of_sibm_samples, m, _N)
     q = Queue()
     process_list = []
     assert(repeat % thread_num == 0)
     inner_repeat = repeat // thread_num
     for _ in range(thread_num):
         t = Process(target=task,
-                    args=(inner_repeat, n, a, b, alpha, beta, num_of_sibm_samples, m, _N, q))
+                    args=(inner_repeat, n, k, a, b, alpha, beta, num_of_sibm_samples, m, _N, q))
         process_list.append(t)
         t.start()
     acc = 0
@@ -132,7 +199,7 @@ if __name__ == "__main__":
     parser.add_argument('--a', type=float, default=16.0)
     parser.add_argument('--b', type=float, default=4.0)
     parser.add_argument('--n', type=int, default=300)
-    # parser.add_argument('--k', type=int, default=2)
+    parser.add_argument('--k', type=int, default=2)
     parser.add_argument('--alpha', type=float, default=8.0)
     parser.add_argument('--beta', type=float, default=1.0, nargs='+')
     parser.add_argument('--repeat', type=int, default=1, help='number of times to generate the SBM graph')
@@ -151,7 +218,7 @@ if __name__ == "__main__":
         beta_list = args.beta
     acc_list = []
     for beta in beta_list:
-        averaged_acc = get_acc(args.repeat, args.n, args.a, args.b,
+        averaged_acc = get_acc(args.repeat, args.n, args.k, args.a, args.b,
                             args.alpha, beta, args.inner_repeat, args.m,
                             args.max_iter, args.thread_num)
         acc_list.append(averaged_acc)
