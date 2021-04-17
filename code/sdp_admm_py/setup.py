2c62a7bccd4f2f1f18d873373aeaca2419c765be
zhaofeng-shu33 <616545598@qq.com>
2021-04-17 11:11:53 +0800

    unified root dir

--- code/sdp_admm_py/setup.py
+++ code/sdp_admm_py/setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import sys
-if sys.path[-1].find('conda') > 0:
-    conda_root = '/home/feng/.conda/envs/r_env/'
-    root_dir = conda_root + 'lib/R/library/'
-else: # debian buster
-    root_dir = '/usr/include/'
+root_dir = '/usr/include/'
 extensions = [
     Extension("sdp_admm_py", ["sdp_admm_py.pyx", "sdp_admm.cpp"],
               include_dirs=[root_dir + 'eigen3']
