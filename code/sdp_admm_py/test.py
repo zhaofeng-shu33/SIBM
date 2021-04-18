cc7d5c178621649b70eda4dd109810ea86ef644a
zhaofeng-shu33 <616545598@qq.com>
2021-04-18 09:08:42 +0800

    add new test

--- code/sdp_admm_py/test.py
+++ code/sdp_admm_py/test.py
@@ -4,7 +4,7 @@ import numpy as np
 import networkx as nx
 
 from sdp_admm_py import sdp1_admm_py, sdp_admm_sbm_2_py
-from utility import get_ground_truth, compare, construct_B
+from utility import get_ground_truth, compare, construct_B, construct_B_tilde, generate_data
 from sbmsdp import sbm2
 
 def get_labels(cluster_matrix):
@@ -21,6 +21,10 @@ def get_labels(cluster_matrix):
        label_index = label_index + 1    
     return labels
 
+def get_labels_sdp2(cluster_matrix):
+    labels = cluster_matrix[0, :] < 0
+    return labels.astype(np.int)
+
 class SDP_ADMM(unittest.TestCase):
     def test_basic(self):
         sizes = [20, 20, 20]
@@ -35,11 +39,30 @@ class SDP_ADMM(unittest.TestCase):
         self.assertAlmostEqual(np.sum(result_label[:20] == result_label[0]), 20)
         self.assertAlmostEqual(np.sum(result_label[20:40] == result_label[20]), 20)
         self.assertAlmostEqual(np.sum(result_label[40:] == result_label[40]), 20)
+
     def test_sdp2(self):
         G = sbm2(100, 16, 4)
         B = construct_B(G)
         results = sdp_admm_sbm_2_py(B, report_interval=10000)
-        result_label = get_labels(results)
+        result_label = get_labels_sdp2(results)
+        labels_true = get_ground_truth(G)
+        self.assertAlmostEqual(compare(result_label, labels_true), 1.0)
+
+    def test_sdp_si_2(self):
+        n = 100
+        m = 10
+        a = 16
+        b = 4
+        G = sbm2(n, a, b)
+        B = construct_B(G)
+        p0 = [0.8, 0.2]
+        p1 = [0.2, 0.8]
+        gt = get_ground_truth(G)
+        a_b_ratio = a / b
+        data = generate_data(gt, n, m, p0, p1)        
+        B_tilde = construct_B_tilde(B, data, p0, p1, a_b_ratio)
+        results = sdp_admm_sbm_2_py(B_tilde, report_interval=10000)
+        result_label = results[0, 1:] > 0
         labels_true = get_ground_truth(G)
         self.assertAlmostEqual(compare(result_label, labels_true), 1.0)
 
