4a5587e8490a5fab8c5a619bc6563a146e789c2d
zhaofeng-shu33 <616545598@qq.com>
2022-09-15 09:06:14 +0800

    update

--- code/explore_sdp2.py
+++ code/explore_sdp2.py
@@ -1,24 +1,43 @@
 import numpy as np
-from sdp import construct_B, project_cone, get_labels_sdp2
-from sbm import sbm_graph, compare, get_ground_truth
+from sdp import construct_B, project_cone
+from sbm import sbm_graph, get_ground_truth
+import matplotlib.pyplot as plt
 
-def sdp2(G, kappa=1.0, rho = 0.1, max_iter = 1000, tol=1e-4):
+def compare(label_1, label_2):
+    n = len(label_1)
+    invert_label_1 = np.ones(n) - label_1
+    acc_1 = np.sum(np.array(label_1) == label_2) / n
+    acc_2 = np.sum(np.array(invert_label_1) == label_2) / n
+    if acc_1 > acc_2:
+        return acc_1
+    return acc_2
+
+def get_labels_sdp2(cluster_matrix):
+    w, v = np.linalg.eigh(cluster_matrix)
+    labels =  v[:, -1] < 0
+    return labels.astype(int)
+
+def sdp2(G, kappa=1.0, rho = 0.1, max_iter = 100, tol=1e-4):
     '''only for two communities
     rho: ADMM penalty parameter
     '''
     B = construct_B(G, kappa)
-
+    np.fill_diagonal(B, 0)
+    gt = get_ground_truth(graph)
     n = len(G.nodes)
     u = np.zeros([n])
     Z = np.zeros([n, n])
+    acc_list = np.zeros(max_iter)
+    residue_list = np.zeros(max_iter)
     for i in range(max_iter):
         Z = project_cone(np.eye(n) + np.diag(u) + B / rho + Z - np.diag(np.diag(Z)))
-        if np.linalg.norm(np.ones([n]) - np.diag(Z)) < tol:
-            break
+        residue_list[i] = np.linalg.norm(np.ones([n]) - np.diag(Z))
         u = u + np.ones([n]) - np.diag(Z)
+        labels = get_labels_sdp2(Z)
+        acc_list[i] = compare(gt, labels)
     import pdb
     pdb.set_trace()
-    return get_labels_sdp2(Z)
+    return acc_list, residue_list
 
 if __name__ == '__main__':
     n = 300
@@ -26,7 +45,8 @@ if __name__ == '__main__':
     a = 16
     b = 4
     graph = sbm_graph(n, k, a, b)
-    labels = sdp2(graph)
-    gt = get_ground_truth(graph)
-    acc = compare(gt, labels)
-    print(acc)
+    acc_list, residue_list = sdp2(graph)
+    plt.plot(acc_list)
+    plt.figure(2)
+    plt.plot(residue_list)
+    plt.show()
